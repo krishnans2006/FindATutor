34de523b752ecea6dbcc73e200980287fbc7ebaf
KrishnanS2006 <krishnans2006@gmail.com>
2020-11-09 00:51:03 -0500

    Committed automatically by run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 cd /site/public
 source venv/bin/activate
-git commit -a --allow-empty-message -m ''
+
+git add *
+git add .
+git commit -m 'Committed automatically by run.sh'
 git push
+
 gunicorn server:app -b 0.0.0.0:80 -w 1
