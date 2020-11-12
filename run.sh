a7a9af8dd70faba1a0ff70235c113c5dee8f871e
KrishnanS2006 <krishnans2006@gmail.com>
2020-11-12 11:05:14 -0500

    Committed automatically by run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ cd /site/public
 source venv/bin/activate
 
 unset GIT_SSH
-git config user.name "KrishnanS2006"
-git config user.password "KsSh101606"
+git config user.name 'KrishnanS2006'
+git config user.password 'KsSh101606'
 git add --all
 git commit -m 'Committed automatically by run.sh'
-git push git@github.com:KrishnanS2006/FindATutor.git
+git push 'git@github.com:KrishnanS2006/FindATutor.git'
 
 gunicorn server:app -b 0.0.0.0:80 -w 1
