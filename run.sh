c49376b00180c30cf565c6e199cbc7922ad2c96d
KrishnanS2006 <krishnans2006@gmail.com>
2020-11-12 10:45:10 -0500

    Committed automatically by run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 cd /site/public
 source venv/bin/activate
 
-
+git config user.name "KrishnanS2006"
+git config user.password "KsSh101606"
 git add --all
 git commit -m 'Committed automatically by run.sh'
 git push git@github.com:KrishnanS2006/FindATutor.git master
