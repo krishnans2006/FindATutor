fa77915bb70a7c9900a4d32d462782d7e4725b53
KrishnanS2006 <krishnans2006@gmail.com>
2020-11-12 10:42:51 -0500

    Committed automatically by run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 cd /site/public
 source venv/bin/activate
 
-git add *
-git add .
+
+git add --all
 git commit -m 'Committed automatically by run.sh'
-git push
+git push git@github.com:KrishnanS2006/FindATutor.git master
 
 gunicorn server:app -b 0.0.0.0:80 -w 1
