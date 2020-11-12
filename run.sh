b2be6f42c76c8093e844633394fc1a98c78221c6
KrishnanS2006 <krishnans2006@gmail.com>
2020-11-12 11:03:11 -0500

    Committed automatically by run.sh

--- run.sh
+++ run.sh
@@ -6,10 +6,8 @@ source venv/bin/activate
 unset GIT_SSH
 git config user.name "KrishnanS2006"
 git config user.password "KsSh101606"
-git remote rm origin
-git remote add origin 'git@github.com:KrishnanS2006/FindATutor.git'
 git add --all
 git commit -m 'Committed automatically by run.sh'
-git push
+git push git@github.com:KrishnanS2006/FindATutor.git
 
 gunicorn server:app -b 0.0.0.0:80 -w 1
