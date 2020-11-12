9aec10ec02f1e999a6f02c28920e9a9594bd37c5
KrishnanS2006 <krishnans2006@gmail.com>
2020-11-12 10:45:59 -0500

    Committed automatically by run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ git config user.name "KrishnanS2006"
 git config user.password "KsSh101606"
 git add --all
 git commit -m 'Committed automatically by run.sh'
-git push git@github.com:KrishnanS2006/FindATutor.git master
+git push git@github.com:FindATutor.git master
 
 gunicorn server:app -b 0.0.0.0:80 -w 1
